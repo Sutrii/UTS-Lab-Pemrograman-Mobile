2b511b49d7e2ec4b53e5699130ee8562e4397c4d
ritsu <sutri4637@gmail.com>
2023-12-05 10:17:28 +0700

    Upload File

---
+++ Android Studio/app/src/main/java/com/sutrii/utslab/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.sutrii.utslab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.sutrii.utslab.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navigasi
+        val navController = findNavController(R.id.nav_host_fragment)
+        navView.setupWithNavController(navController)
+    }
+}
